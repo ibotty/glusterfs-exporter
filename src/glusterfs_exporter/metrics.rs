dc2603476e6f9131584fecd1b429c3c46368827e
Tobias Florek <tob@butter.sh>
2016-11-03 21:48:39 +0100

    properly initialize BLOCKSTATS_COUNTER metric

--- src/glusterfs_exporter/metrics.rs
+++ src/glusterfs_exporter/metrics.rs
@@ -4,7 +4,7 @@ lazy_static! {
     pub static ref EXPORTER_FAILURE_COUNTER: Counter = register_counter!("glusterfs_failures", "Failures internal to glusterfs_exporter").unwrap();
 
     pub static ref PROFILE_OP_GAUGE: GaugeVec = register_gauge_vec!("glusterfs_profile_ops", "Glusterfs volume profile Ops", &["volume"]).unwrap();
-    pub static ref BLOCKSTATS_COUNTER: GaugeVec = register_gauge_vec!("glusterfs_blockstats", "Glusterfs volume block stats", &["volume", "optype", "size_bucket"]).unwrap();
+    pub static ref BLOCKSTATS_COUNTER: GaugeVec = register_gauge_vec!("glusterfs_blockstats", "Glusterfs volume block stats", &["volume", "brick", "optype", "size_bucket"]).unwrap();
     pub static ref BLOCKSTATS_TOTAL_COUNTER: GaugeVec = register_gauge_vec!("glusterfs_blockstats_total", "Glusterfs volume total block stats", &["volume", "brick", "optype"]).unwrap();
     pub static ref FOPSTATS_AVGLATENCY: GaugeVec = register_gauge_vec!("glusterfs_fop_avglatency", "Glusterfs volume file operation avg latency", &["volume", "brick", "fop_name"]).unwrap();
     pub static ref FOPSTATS_MINLATENCY: GaugeVec = register_gauge_vec!("glusterfs_fop_minlatency", "Glusterfs volume file operation min latency", &["volume", "brick", "fop_name"]).unwrap();
